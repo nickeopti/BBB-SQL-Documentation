858e080d176cc0fddd47401379793bd88f4cf15f
Nicklas Boserup <nickeopti@gmail.com>
2019-02-07 10:36:54 +0100

    Update and simplify queries

--- Queries/Table Home/Daily.sql
+++ Queries/Table Home/Daily.sql
@@ -1,54 +1,91 @@
+-- Check the newer version below this query
+
 SELECT
-    b.gid,
     b.id,
-    a.zone_act,
-    a.days_act,
-    c.name_day,
-    a.hours_act,
-    a.count_act,
+    a.zone,
+    a.day,
+    c.description,
+    a.hour,
+    a.people,
     b.geom
 FROM mtc_activity a 
 INNER JOIN mtc b
-    ON a.zone_act = b.gid
-INNER JOIN days c
-    ON a.days_act = c.id_day
+    ON a.zone = b.id
+INNER JOIN day c
+    ON a.day = c.id
 WHERE
-    count_act = (
+    people = (
         SELECT
-            MAX(count_act)
+            MAX(people)
         FROM mtc_activity AS f 
         WHERE
-            f.zone_act = a.zone_act AND
-            f.days_act = a.days_act
+            f.zone = a.zone AND
+            f.day = a.day
     ) AND
-    ST_Contains(b.geom, ST_GeomFromText('POINT(11.56330 48.18674)', 4326))=true AND
-    a.days_act=1
+	a.zone = 145 AND
+    --ST_Contains(b.geom, ST_GeomFromText('POINT(11.56330 48.18674)', 4326))=true AND
+    a.day = 1
 
 UNION
 
 SELECT
-    b.gid,
     b.id,
-    a.zone_act,
-    a.days_act,
-    c.name_day,
-    a.hours_act,
-    a.count_act,
+    a.zone,
+    a.day,
+    c.description,
+    a.hour,
+    a.people,
     b.geom
 FROM mtc_activity a 
 INNER JOIN mtc b
-    ON a.zone_act = b.gid
-INNER JOIN days c
-    ON a.days_act = c.id_day
+    ON a.zone = b.id
+INNER JOIN day c
+    ON a.day = c.id
 WHERE
-    count_act = (
+    people = (
         SELECT
-            MIN(count_act)
+            MIN(people)
         FROM mtc_activity AS f 
         WHERE
-            f.zone_act = a.zone_act AND
-            f.days_act = a.days_act
+            f.zone = a.zone AND
+            f.day = a.day
     ) AND
-    ST_Contains(b.geom, ST_GeomFromText('POINT(11.56330 48.18674)', 4326))=true AND
-    a.days_act = 1
-ORDER BY count_act DESC;
+	a.zone = 145 AND
+    --ST_Contains(b.geom, ST_GeomFromText('POINT(11.56330 48.18674)', 4326))=true AND
+    a.day = 1
+ORDER BY people DESC;
+
+
+-- New alternative that needs verification
+
+SELECT
+	day.id AS day,
+	day.description,
+	act.hour,
+	act.people
+FROM mtc_activity AS act
+INNER JOIN day
+	ON act.day = day.id
+WHERE
+	zone = 145 AND
+	day = 1 AND
+	(
+		people = (
+			SELECT
+				MAX(people)
+			FROM mtc_activity
+			WHERE
+				zone = 145 AND
+				day = 1
+		) OR
+		people = (
+			SELECT
+				MIN(people)
+			FROM mtc_activity
+			WHERE
+				zone = 145 AND
+				day = 1
+		)
+	)
+ORDER BY people DESC;
+
--- Queries/Table Home/Weekly.sql
+++ Queries/Table Home/Weekly.sql
@@ -1,3 +1,5 @@
+-- Check the newer version below this query
+
 SELECT
     b.gid,
     b.id,
@@ -55,4 +57,36 @@ HAVING
             a.zone_act = 145
         GROUP BY a.zone_act,a.days_act
     )
+ORDER BY people DESC;
+
+
+-- New alternative that needs verification
+
+SELECT
+	day.id AS day,
+	day.description,
+	SUM(act.people) AS people
+FROM mtc_activity AS act
+INNER JOIN day
+	ON act.day = day.id
+WHERE
+	zone = 145
+GROUP BY day.id
+HAVING
+	SUM(people) >= ALL (
+		SELECT
+			SUM(people)
+		FROM mtc_activity
+		WHERE
+			zone = 145
+		GROUP BY day
+	) OR
+	SUM(people) <= ALL (
+		SELECT
+			SUM(people)
+		FROM mtc_activity
+		WHERE
+			zone = 145
+		GROUP BY day
+	)
 ORDER BY people DESC;
