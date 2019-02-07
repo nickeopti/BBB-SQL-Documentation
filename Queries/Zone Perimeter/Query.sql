19e3588d855532c65d296ffbe11c385acf72006e
Nicklas Boserup <nickeopti@gmail.com>
2019-02-07 19:08:25 +0100

    Implement lat/lon query

--- Queries/Zone Perimeter/Query.sql
+++ Queries/Zone Perimeter/Query.sql
@@ -7,3 +7,17 @@ FROM (
     FROM mtc
     WHERE id = $1
 ) s;
+
+
+-- Alternative without zone id
+
+SELECT
+    ST_X(point) AS lon,
+    ST_Y(point) AS lat
+FROM (
+    SELECT
+        (ST_Dump(ST_RemoveRepeatedPoints(ST_Points(geom)))).geom AS point
+    FROM mtc
+    WHERE
+        ST_Contains(geom, ST_SetSRID(ST_MakePoint($1, $2), 4326))
+) s;
