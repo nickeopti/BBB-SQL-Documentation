fee139762b6bf9865ed279cc925aeebe872f79fc
Nicklas Boserup <nickeopti@gmail.com>
2019-02-07 14:58:58 +0100

    Implement lat/lon query

--- Queries/Table Home/Weekly.sql
+++ Queries/Table Home/Weekly.sql
@@ -6,7 +6,7 @@ FROM mtc_activity AS act
 INNER JOIN day
     ON act.day = day.id
 WHERE
-    zone = 145
+    zone = $1
 GROUP BY day.id
 HAVING
     SUM(people) >= ALL (
@@ -14,7 +14,7 @@ HAVING
             SUM(people)
         FROM mtc_activity
         WHERE
-            zone = 145
+            zone = $1
         GROUP BY day
     ) OR
     SUM(people) <= ALL (
@@ -22,7 +22,45 @@ HAVING
             SUM(people)
         FROM mtc_activity
         WHERE
-            zone = 145
+            zone = $1
+        GROUP BY day
+    )
+ORDER BY people DESC;
+
+
+-- Alternative without zone id
+
+SELECT
+    day.id AS day,
+    day.description,
+    SUM(act.people) AS people
+FROM mtc_activity AS act
+INNER JOIN day
+    ON act.day = day.id
+INNER JOIN mtc AS zone
+    ON act.zone = zone.id
+WHERE
+    ST_Contains(zone.geom, ST_SetSRID(ST_MakePoint($1, $2), 4326))
+GROUP BY day.id
+HAVING
+    SUM(people) >= ALL (
+        SELECT
+            SUM(people)
+        FROM mtc_activity AS act
+        INNER JOIN mtc AS zone
+            ON act.zone = zone.id
+        WHERE
+            ST_Contains(zone.geom, ST_SetSRID(ST_MakePoint($1, $2), 4326))
+        GROUP BY day
+    ) OR
+    SUM(people) <= ALL (
+        SELECT
+            SUM(people)
+        FROM mtc_activity AS act
+        INNER JOIN mtc AS zone
+            ON act.zone = zone.id
+        WHERE
+            ST_Contains(zone.geom, ST_SetSRID(ST_MakePoint($1, $2), 4326))
         GROUP BY day
     )
 ORDER BY people DESC;
