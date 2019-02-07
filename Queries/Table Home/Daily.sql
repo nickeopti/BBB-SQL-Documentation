eaf33706f612388dffa333a99b0dbde68a7d9a6a
Nicklas Boserup <nickeopti@gmail.com>
2019-02-07 10:42:17 +0100

    Fix spacing

--- Queries/Table Home/Daily.sql
+++ Queries/Table Home/Daily.sql
@@ -59,33 +59,32 @@ ORDER BY people DESC;
 -- New alternative that needs verification
 
 SELECT
-	day.id AS day,
-	day.description,
-	act.hour,
-	act.people
+    day.id AS day,
+    day.description,
+    act.hour,
+    act.people
 FROM mtc_activity AS act
 INNER JOIN day
-	ON act.day = day.id
+    ON act.day = day.id
 WHERE
-	zone = 145 AND
-	day = 1 AND
-	(
-		people = (
-			SELECT
-				MAX(people)
-			FROM mtc_activity
-			WHERE
-				zone = 145 AND
-				day = 1
-		) OR
-		people = (
-			SELECT
-				MIN(people)
-			FROM mtc_activity
-			WHERE
-				zone = 145 AND
-				day = 1
-		)
-	)
+    zone = 145 AND
+    day = 1 AND
+    (
+        people = (
+            SELECT
+                MAX(people)
+            FROM mtc_activity
+            WHERE
+                zone = 145 AND
+                day = 1
+        ) OR
+        people = (
+            SELECT
+                MIN(people)
+            FROM mtc_activity
+            WHERE
+                zone = 145 AND
+                day = 1
+        )
+    )
 ORDER BY people DESC;
-
--- Queries/Table Home/Weekly.sql
+++ Queries/Table Home/Weekly.sql
@@ -63,30 +63,30 @@ ORDER BY people DESC;
 -- New alternative that needs verification
 
 SELECT
-	day.id AS day,
-	day.description,
-	SUM(act.people) AS people
+    day.id AS day,
+    day.description,
+    SUM(act.people) AS people
 FROM mtc_activity AS act
 INNER JOIN day
-	ON act.day = day.id
+    ON act.day = day.id
 WHERE
-	zone = 145
+    zone = 145
 GROUP BY day.id
 HAVING
-	SUM(people) >= ALL (
-		SELECT
-			SUM(people)
-		FROM mtc_activity
-		WHERE
-			zone = 145
-		GROUP BY day
-	) OR
-	SUM(people) <= ALL (
-		SELECT
-			SUM(people)
-		FROM mtc_activity
-		WHERE
-			zone = 145
-		GROUP BY day
-	)
+    SUM(people) >= ALL (
+        SELECT
+            SUM(people)
+        FROM mtc_activity
+        WHERE
+            zone = 145
+        GROUP BY day
+    ) OR
+    SUM(people) <= ALL (
+        SELECT
+            SUM(people)
+        FROM mtc_activity
+        WHERE
+            zone = 145
+        GROUP BY day
+    )
 ORDER BY people DESC;
